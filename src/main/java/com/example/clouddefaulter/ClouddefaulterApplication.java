bcf993d3876624d5041fad76631723570a52b335
Kshitij Mahale <kshitijmahale02@gmail.com>
2025-11-20 18:53:28 +0530

    First commit

---
+++ src/main/java/com/example/clouddefaulter/ClouddefaulterApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouddefaulter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddefaulterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouddefaulterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clouddefaulter/ClouddefaulterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clouddefaulter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouddefaulterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
